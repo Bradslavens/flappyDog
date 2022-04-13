ca968d11b1fb9c9b2f8b9ebfc932770f3d0e71f6
BradSlavens <bradslavens@gmail.com>
2022-04-13 06:50:37 -0700

    jumping but not correctly

--- myJs.js
+++ myJs.js
@@ -8,22 +8,52 @@
 // controls
 const JUMP_HEIGHT = 10
 const RANGE = JUMP_HEIGHT
+const JUMP_SPEED = 1000
+let jumper = 0
 
 
 let player = document.getElementById("player")
 window.addEventListener('keydown', jump)
 
 function jump(e){
-    if(e.code == 'Space'){
-        let heightIncrement = 0
-        for(let i = -JUMP_HEIGHT; i<=JUMP_HEIGHT; i++){
-            console.log((-(i**2)) + RANGE**2)
-            // player.style.marginTop = "-100px"
-            // console.log(player.offsetTop)
+    if(e.code = "Space"){
+        for(let i=-10; i<=10; i++){
+            sto(i)
+        }
+
+        function sto(i){
+            setTimeout(jumper, 50 * i, i)
+        }
+        let offset = 50
+        function jumper(i){
             // heightIncrement = (-(i**2)) + RANGE**2
-            heightIncrement+= 10
-            player.style.marginTop = `-${heightIncrement}px`
-            console.log(` margin top ${window.getComputedStyle(player, null).getPropertyValue("margin-top")}`)
+            let heightIncrement = ((-(i**2)) + RANGE**2) + offset
+            console.log(heightIncrement)
+            player.style.bottom = `${heightIncrement}px`
         }
     }
+//     if(e.code == 'Space'){
+//         for(let i = 0; i< 10; i++){
+//             (function(i){
+//                 jumper = setInterval(() => {
+//                     console.log(`hello ${i}`)
+//                 }, 1000);
+//             })(i)
+//         }
+//         clearInterval(jumper)
+        // let heightIncrement = 0
+        // let i = -JUMP_HEIGHT
+        // i<=JUMP_HEIGHT
+        // i++
+        // jumper = setInterval(jumpArc, JUMP_SPEED);
+    // }
 }
+
+// let heightIncrement = 0
+// let i = -JUMP_HEIGHT
+// function jumpArc(){
+//     heightIncrement = (-(i**2)) + RANGE**2
+//     player.style.bottom = `${heightIncrement}px`
+//     i++
+//     if(i<=JUMP_HEIGHT) clearInterval(jumper)
+// }
