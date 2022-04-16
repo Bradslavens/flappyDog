096d38f5897dbdd0b18ed512e167fa6e58438ea6
BradSlavens <bradslavens@gmail.com>
2022-04-15 21:22:10 -0700

    changed fps to 60 to smooth animation

--- myJs.js
+++ myJs.js
@@ -7,12 +7,9 @@
 
 // controls
 const JUMP_HEIGHT = 10
-const RANGE = JUMP_HEIGHT
 const JUMP_SPEED = 1000
 const JUMP_FACTOR = 20
-const JUMP_DELAY = 50
-let jumper = 0
-let jumpInterval = 14
+let jumpInterval = 16.66
 
 
 let player = document.getElementById("player")
@@ -23,18 +20,13 @@ function handleKeydown(e){
     if(e.code === "Space"){
         if(jumpId !== 0){ clearInterval(jumpId)}
         let offset = window.getComputedStyle(player, null).getPropertyValue("bottom").match(regex)
-        if(offset <= 50) offset = 50
         offset = Number(offset)
         let i = -10
-        let speed = 0
         jumpId = setInterval(() => {
-            if(speed%10 == 0){
-                let heightIncrement = ((-(i**2))) + RANGE**2 + offset
+                if(offset <= 49.9) clearInterval(jumpId)
+                let heightIncrement = ((-(i**2))) + JUMP_HEIGHT**2 + offset
                 player.style.bottom = `${heightIncrement}px`
-                i++
-            }
-            
-            speed++
+                i+= .25
         }, jumpInterval);
     }
 
