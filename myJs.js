7894e250a9486617130c561af994c150d3a8f025
BradSlavens <bradslavens@gmail.com>
2022-04-15 22:58:49 -0700

    got targets spawning ... but its really working my computer

--- myJs.js
+++ myJs.js
@@ -9,9 +9,10 @@
 const JUMP_HEIGHT = 10
 const JUMP_SPEED = 1000
 const JUMP_FACTOR = 20
-let jumpInterval = 16.66
+const FPS = 60
+let fpsms = 1000/FPS
 
-
+/// handle player jumping 
 let player = document.getElementById("player")
 window.addEventListener('keydown', handleKeydown)
 let jumpId = 0
@@ -30,7 +31,53 @@ function handleKeydown(e){
             console.log(heightIncrement)
             // console.log(`player style bottom ${player.style.bottom}`)
             // if(player.style.bottom.match(regex)[0] <= 50) clearInterval(jumpId)
-        }, jumpInterval);
+        }, fpsms);
     }
 
 }
+
+// game manager
+let gmIntervalId = 0
+let loopCounter = 0
+let targetIncrement = .50
+let arena = document.getElementById("arena")
+const targetSpeed = 1
+let targetLoopCount = 0
+
+function start() {
+    gmIntervalId= setInterval(gameManager, fpsms)
+}
+
+function stop() {
+    console.log("stop")
+    clearInterval(gmIntervalId)
+}
+
+function gameManager(){
+    if(loopCounter%100 == 0) spawnTarget()
+    moveTarget()
+    loopCounter++
+
+}
+
+function moveTarget(){
+    let targets = document.querySelectorAll(".target")
+    for(let i = 0; i<targets.length; i++){
+        let posLeft = targets[i].offsetLeft
+        console.log(typeof(posLeft))
+        console.log(`pos left : ${posLeft} target speed: ${targetSpeed}`)
+        let posL = posLeft - targetSpeed
+        targets[i].style.left = `${posL}px`
+    }
+}
+
+function spawnTarget(){
+    const target = document.createElement("div")
+    console.log(typeof(target))
+    target.setAttribute("class", "target")
+    let height = Math.floor(Math.random() * 90)
+    target.style.bottom = `${height}%`
+    arena.insertAdjacentElement("afterbegin", target)
+}
+
+
